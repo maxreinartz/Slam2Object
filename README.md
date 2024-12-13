d42cf748972da1a491299e9fa27b81f95cfd9218
Creeper76 <86135033+Creeper76@users.noreply.github.com>
2024-12-12 19:30:02 -0700

    Add README and LICENSE

---
+++ README.md
@@ -0,0 +1,10 @@
+# Slam2Object
+
+## Overview
+A fun website which triggers an "Objection!" when you make a loud noise (like slamming a table!)
+
+## Contributing
+Contributions are welcome! Please fork the repository and submit a pull request.
+
+## License
+This project is licensed under the MIT License.
--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,21 @@
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('slam2object-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/app.js',
-                '/manifest.json',
-                '/icon.png'
-            ]);
-        })
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("slam2object-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/app.js",
+        "/manifest.json",
+        "/icon.png",
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
